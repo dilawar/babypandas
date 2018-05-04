ea297f4039cdc7c9205b58fcec8f501ffefc017f
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-04 11:48:24 +0530

    Added setup file. Not sure about LICENSE.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+classifiers = [
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    ]
+
+setup(
+    name = "babypandas",
+    version = "0.0.1",
+    description = "A small pure python library with Pandas like API"
+    long_description = readme,
+    package_data = {},
+    install_requires = [ ],
+    author = "aegorenkov",
+    maintainer = "Dilawar Singh",
+    maintainer_email = "dilawars@ncbs.res.in"
+    url = "http://github.com/dilawar/",
+    license='GPL',
+    classifiers=classifiers,
+)
